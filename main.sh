2d8fb3ea875c8902a91239ed3239bfcd55d263bf
aMs772 <adusumallimohansatvik@gmail.com>
2026-03-24 21:03:33 +0530

    Add login function for user authentication
    Implement login function with username and password validation.

--- main.sh
+++ main.sh
@@ -1,1 +1,37 @@
+#!/usr/bin/bash
 
+player1=""  
+
+login() {
+        while true; do
+                read -p "Enter username: " name
+           if [[ $player1 == $name ]]; then                   
+                   echo "Nice try"
+                   echo "You can not play with yourself"
+                   continue
+           else
+                if grep -q "^$name[[:space:]]" users.tsv; then  
+                        while true; do
+                                read -sp "Enter password: " password
+                                echo ""
+                                password_hash=$( echo -n "$password" | sha256sum | awk '{print $1}' )
+                                if grep -q "^$name[[:space:]]$password_hash" users.tsv; then
+                                        echo "login of player $name is successful"
+                                        player=$name
+                                        break 2
+                                else
+                                        echo "Nice try"
+                                        read -p "Is your username $name?(y/n)" truth
+                                        if [[ $truth == "y" || $truth == "Y" ]]; then
+                                                continue
+                                        else
+                                                break
+                                        fi
+                                fi
+                        done
+                else   ### need to do registration part.
+                fi
+           fi
+        done
+  }
+                        
